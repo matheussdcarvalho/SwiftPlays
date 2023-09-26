e7b6ec5ae4e9ada32d546fbc0945aa39f02a56b3
Matheus Carvalho <124722630+matheussdcarvalho@users.noreply.github.com>
2023-09-26 15:00:13 -0300

    Commit aplicativo lista de tarefas (incompleto)

---
+++ listaTarefas/listaTarefas/TaskView.swift
@@ -0,0 +1,71 @@
+//
+//  TaskView.swift
+//  listaTarefas
+//
+//  Created by User on 22/09/23.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        
+        VStack{
+            Text("LISTA DE TAREFAS")
+                .font(.largeTitle)
+                .offset(y: -260)
+            
+            // Tarefa 1
+            label: do {
+                Text("TAREFA 1")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 130)
+                        .background(Color.white)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                        .offset(y: -250)
+                }
+            // Tarefa 2
+            label: do {
+                Text("TAREFA 2")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 130)
+                        .background(Color.white)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                        .offset(y: -240)
+                }
+            // Tarefa 3
+            label: do {
+                Text("TAREFA 3")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 130)
+                        .background(Color.white)
+                        .cornerRadius(50)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 50)
+                                .stroke(Color.black, lineWidth: 2)
+                        )
+                        .offset(y: -229)
+                }
+            }
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
---
+++ listaTarefas/listaTarefas/listaTarefasApp.swift
@@ -0,0 +1,17 @@
+//
+//  listaTarefasApp.swift
+//  listaTarefas
+//
+//  Created by User on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct listaTarefasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
